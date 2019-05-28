d73ea0042d19d0fbf8e11d0e65954bfea5e0134e
naaspati <naaspati@gmail.com>
2019-05-28 17:52:26 +0530

    minor

--- js/index.js
+++ js/index.js
@@ -1,35 +1,11 @@
 const player = document.getElementById('audio_player');
 const dialedNumber = document.getElementById("dialed_number");
 
-function previous() {
-    console.log("previous");
-}
-function play() {
-    player.play();
-}
-function pause() {
-    player.pause();
-}
-function next() {
-    console.log("next");
-}
-function stop() {
-    console.log("stop");
-}
-function keypadClick(n) {
-    n = n.target.innerText;
-    if (n !== '*' && n !== '#')
-        dialedNumber.innerText = dialedNumber.innerText + n;
-}
-function dialAction() {
-    console.log("dialAction");
-}
+const playBtn = document.getElementsByClassName("btn play")[0];
+const pauseBtn = document.getElementsByClassName("btn pause")[0];
 
-function backspace() {
-    const s = dialedNumber.innerText;
-    if (s)
-        dialedNumber.innerText = s.substring(0, s.length - 1);
-}
+const rightSide = document.getElementById('player');
+const volume_slider = document.getElementById('volume_slider');
 
 const bars = Array.from(document.getElementById('visualization').getElementsByClassName('bar'));
 const colors = [
@@ -45,8 +21,126 @@ const colors = [
     "#FF6A63",
     "#F82D98",
     "#E830CE"
-  ];
-  
+];
 
+const music_name = document.getElementById('playing_name');
 const renderer = new Renderer();
-renderer.start();
+
+class Progress {
+    constructor() {
+        const e = document.getElementById('progress_container');
+        this.progress = e.getElementsByTagName('progress')[0];
+        [this._passedTime, this._duration] = e.getElementsByTagName('span');
+    }
+
+    set timePassed(seconds) {
+        this._passedTime.innerText = secondsToHms(seconds);
+        this.progress.value = seconds;
+    }
+    set duration(seconds) {
+        this.timePassed = 0;
+        this._duration.innerText = secondsToHms(seconds);
+        this.progress.max = seconds;
+    }
+}
+
+function secondsToHms(d) {
+    d = Math.floor(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = h > 0 ? (h < 10 ? '0' : '') + h + '/' : "";
+    var mDisplay = (m < 10 ? '0' : '') + m + '/';
+    var sDisplay = (s < 10 ? '0' : '') + s;
+    return hDisplay + mDisplay + sDisplay;
+}
+
+const progress = new Progress();
+
+player.addEventListener('durationchange', function () {
+    progress.duration = this.duration;
+});
+
+player.addEventListener('play', () => play(true));
+player.addEventListener('pause', () => pause(true));
+
+player.addEventListener('timeupdate', function () {
+    progress.timePassed = this.currentTime;
+});
+
+function previous() {
+    console.log("previous");
+}
+
+function play(playerStopped) {
+    if (!playerStopped)
+        player.play();
+    else {
+        playBtn.style.display = 'none';
+        pauseBtn.style.display = 'inline';
+
+        renderer.start();
+    }
+
+}
+function pause(playerStopped) {
+    if (!playerStopped)
+        player.pause();
+    else {
+        playBtn.style.display = 'inline';
+        pauseBtn.style.display = 'none';
+
+        renderer.stop();
+    }
+
+}
+function next() {
+    console.log("next");
+}
+function stop() {
+    player.pause();
+    player.currentTime = 0;
+}
+function keypadClick(n) {
+    n = n.target.innerText;
+
+    if (n !== '*' && n !== '#' && (!dialedNumber.innerText || dialedNumber.innerText.length < 10))
+        dialedNumber.innerText = dialedNumber.innerText + n;
+}
+function dialAction() {
+    let k = dialedNumber.innerText;
+    if(!k) {
+        alert('no value specified');
+        return;
+    }
+
+    let song = musicFiles[Number(k)];
+
+    if(!song) {
+        alert('no music file found for: '+k);
+        return;
+    }
+
+    load('./audio/85bpm/'+song);
+    play();
+}
+
+function backspace() {
+    const s = dialedNumber.innerText;
+    if (s)
+        dialedNumber.innerText = s.substring(0, s.length - 1);
+}
+
+function load(file) {
+    player.src = file;
+    player.load();
+    player.play();
+    music_name.innerText = file;
+}
+
+player.volume = Number(volume_slider.value) / 100;
+volume_slider.addEventListener('change', function () {
+    player.volume = Number(volume_slider.value) / 100;
+});
+
--- js/random-equalizer.js
+++ js/random-equalizer.js
@@ -9,27 +9,36 @@ class Renderer {
     }
 
     render() {
-        for (let m = 0; m < bars.length;  m++) {
+        for (let m = 0; m < bars.length; m++) {
             const e = bars[m];
 
-            e.style.height = heights[Math.floor(Math.random() * 70)];
-            e.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+            const c = Math.floor(Math.random() * 80);
+            e.style.height = heights[c];
+            e.style.backgroundColor = colors[c % colors.length];
         }
     }
 
     start() {
-        for (const e of document.getElementsByClassName('bar')) 
-            e.style.transition =  'height .35s ease-in, background-color .35s ease-in'; 
-        
+        for (const e of document.getElementsByClassName('bar'))
+            e.style.transition = 'height .35s ease-in, background-color .35s ease-in';
+
         this.render();
-        this.ref = setInterval(this.nextFrame, 500);
+        this.ref = setInterval(this.nextFrame, 300);
     }
     stop() {
         if (this.ref) {
             clearInterval(this.ref);
             delete this.ref;
+
+            for (const e of document.getElementsByClassName('bar'))
+                e.style.transition = 'height 1s ease-in, background-color .35s ease-in';
+
+            for (let m = 0; m < bars.length; m++) {
+                const e = bars[m];
+                e.style.height = heights[0];
+            }
         }
-            
+
     }
 }
 
